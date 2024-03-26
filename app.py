590b9ba7ac745844e5bace25116c567a7fa720be
CommanderCodePro <mahaja@pedarecc.sa.edu.au>
2024-03-26 13:06:22 +1030

    Weather Data for 0

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template, requests
+from flask import Flask, render_template, request
 from datetime import datetime
-from datetime import datetimeimport requests
 from matplotlib import pyplot as plt
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
+    api_key = "8649ce5dac17fa487efbd14f7cd30f9e"
+    city = "http://ip-api.com/json/"
+    location_response = requests.get(city).json().get("city")
+    url = "https://api.openweathermap.org/data/2.5/forecast?q=" + location_response + "&APPID=" + api_key
+    print(url)
+    response = requests.get(url).json()
+    print(response)
+
+    weather_data = [0]
+    for item in response['list']:
+        'timestamp': item['dt'],
+        'temperature': item['main']['temp'],
+        'description': item['weather'][0]['description'],
+        'icon': item['weather'][0]['icon']
+
+
     return render_template('home.html')
 
 
 if __name__ == '__main__':
-        app.run(debug=True)
+    app.run(debug=True)
