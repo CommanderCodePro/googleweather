e7c6ab6aeaf6514fc5235b5217d9c7c33a47a185
CommanderCodePro <mahaja@pedarecc.sa.edu.au>
2024-04-02 13:00:12 +1030

    uploading to assignment

--- app.py
+++ app.py
@@ -15,16 +15,43 @@ def home():
     response = requests.get(url).json()
     print(response)
 
-    weather_data = [0]
-    for item in response['list']:
-        'timestamp': item['dt'],
-        'temperature': item['main']['temp'],
-        'description': item['weather'][0]['description'],
-        'icon': item['weather'][0]['icon']
+    # weather_data = [0]
+    # for item in response['list']:
+    #     timestamp': item['dt'],
+    #     temperature': item['main']['temp'],
+    #     description': item['weather'][0]['description'],
+    #     icon': item['weather'][0]['icon']
+
+    weather_data = response['list'];
+    print(weather_data)
+    timestamp = response.get('dt_txt')[0],
+    #Temperature
+    temp_min_k = response.get('main')[0].get('temp_min')
+    temp_min_c = temp_min_k - 273.15
+    temp_max_k = response.get('main')[0].get('temp_max')
+    temp_max_c = temp_max_k - 273.15
+    temp_k = response.get('main')[0].get('temp')
+    temp_c = temp_k - 273.15
+    #Additional
+    wind_speed = response.get('wind').get('speed')
+    humidity = response.get('main').get('humidity')
+    description = response.get('weather')[0].get('description')
+    #Icon
+    icon = response.get('weather')[0].get('icon')
+    icon_url = "http://openweathermap.org/img/w/" + icon + ".png";
+    my_list = [api_key, city, location_response, url, response, weather_data, timestamp, temp_min_c, temp_max_c,temp_c, description,
+                           wind_speed, humidity, icon, icon_url]
+    print(my_list)
+
+    forecast_list = response['list']
+    print(forecast_list)
+
+
+    for forecast in forecast_list:
+        print(forecast['dt_txt'])
 
 
     return render_template('home.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
