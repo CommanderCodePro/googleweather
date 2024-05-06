f5f92820fbbebf17565ee1fa85b870f86fa8b0a4
CommanderCodePro <mahaja@pedarecc.sa.edu.au>
2024-05-06 22:27:00 +0930

    Dictionary created for major parts of the data

--- app.py
+++ app.py
@@ -20,24 +20,25 @@ def home():
     # print(response)
 
     # Weather Data
-    location = response_2.get('name'),
-    timezone = response_2.get('timezone')
-    timestamp =response_2.get('dt')
-    date_time = datetime.fromtimestamp(timestamp)
-    local_timestamp = ""
-    description = response_2.get('weather')[0].get('description')
-    temp_k = response_2.get('main').get('temp')
-    temp_c = temp_k - 273.15
-    wind_speed = response_2.get('wind').get('speed')
-    icon = response_2.get('weather')[0].get('icon')
-    icon_url = "http://openweathermap.org/img/w/" + icon + ".png"
+    weather_location = response_2.get('name'),
+    weather_timezone = response_2.get('timezone')
+    weather_timestamp =response_2.get('dt')
+    weather_date_time = datetime.fromtimestamp(timestamp)
+    weather_local_timestamp = ""
+    weather_description = response_2.get('weather')[0].get('description')
+    weather_temp_k = response_2.get('main').get('temp')
+    weather_temp_c = temp_k - 273.15
+    weather_wind_speed = response_2.get('wind').get('speed')
+    weather_icon = response_2.get('weather')[0].get('icon')
+    weather_icon_url = "http://openweathermap.org/img/w/" + icon + ".png"
 
-    my_list = [api_key, city, weather_url, response_2, location, timezone, timestamp, date_time, local_timestamp, description,
-               temp_k, temp_c, wind_speed, icon, icon_url]
+    my_list = [api_key, city, weather_url, response_2, weather_location, weather_timezone, weather_timestamp, weather_date_time, weather_local_timestamp, weather_description,
+               weather_temp_k, weather_temp_c, weather_wind_speed, weather_icon, weather_icon_url]
     print(my_list)
 
 
     # Forecast Data
+    forecast_data = []
     index = 0
     forecast_list = response_1['list']
     print("************************\n")
@@ -48,21 +49,49 @@ def home():
         print(forecast_list[index]['dt_txt'])
         # Put here how to do the day
         # Temperature
-        print(forecast_list[index]['main']['temp'] - 273.15)
-        print(forecast_list[index]['main']['temp_max'] - 273.15)
-        print(forecast_list[index]['main']['temp_max'] - 273.15)
-        print(forecast_list[index]['main']['feels_like'] - 273.15)
-        # Additional
-        print(forecast_list[index]['wind']['speed'])
-        print(forecast_list[index]['main']['humidity'])
-        print(forecast_list[index]['main']['pressure'])
+        # print(forecast_list[index]['main']['temp'] - 273.15)
+        # forecast_list[index]['main']['temp_max'] - 273.15)
+        # forecast_list[index]['main']['temp_min'] - 273.15)
+        # forecast_list[index]['main']['feels_like'] - 273.15)
+        # # Additional
+        # forecast_list[index]['wind']['speed'])
+        # (forecast_list[index]['main']['humidity'])
+        # (forecast_list[index]['main']['pressure'])
         # Description and Icon
-        print(forecast_list[index]['weather'][0]['description'])
-        print(forecast_list[index]['weather'][0]['icon'])
-        print("http://openweathermap.org/img/w/" + icon + ".png")
-        index += 8
+        dt_text = forecast_list[index]['dt_txt']
+        temp = round(forecast_list[index]['main']['temp'] - 273.15, 2)
+        temp_max = round(forecast_list[index]["main"]['temp_max'] - 273.15, 2)
+        temp_min = round(forecast_list[index]["main"]['temp_min'] - 273.15, 2)
+        # Put here how to do the day
+        # Temperature
+        description = forecast_list[index]['weather'][0]['description']
+        icon = forecast_list[index]['weather'][0]['icon']
 
-    return render_template('home.html')
+
+        # Convert the string to a datetime object
+        date_object = datetime.strptime(dt_text, '%Y-%m-%d %H:%M:%S')
+        # Get the day of the week (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
+        day_of_week = date_object.weekday()
+        print(day_of_week)
+        # You can also get the day name
+        day_name = date_object.strftime('%A')
+        print(day_name)
+
+
+        dict = {
+            "dt_text": dt_text,
+            "temp": temp,
+            "temp_max": temp_max,
+            "temp_min": temp_min,
+            "description": description,
+            "icon_url": "http://openweathermap.org/img/w/" + icon + ".png"
+
+        }
+        forecast_data.append(dict)
+        index += 8
+    print(forecast_data)
+
+    return render_template('home.html', forecast_data = forecast_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
